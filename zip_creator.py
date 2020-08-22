a367e9d94310beb35d0ddd227d085d547fb6d9ee
Tanishk Sharma <tanishk9597@gmail.com>
2020-08-23 02:10:07 +0530

    boilerplate code only for now....
    Thinking.... Thinking.....

---
+++ zip_creator.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+from datetime import datetime
+import logging
+
+
+class ZipCreator:
+    def __init__(self, source, destination):
+       pass
+
+    def create_zip(self):
+      pass
+
+
