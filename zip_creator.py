bfd14c2b9407876bf9c98494bf7f37c302db8828
Tanishk Sharma <tanishk9597@gmail.com>
2020-08-25 14:35:13 +0530

    __init__ complete
    Keeping it very simple - the initialization takes only the source and the destination paths.
    Also trying out logging module in this one ;)
    (NOTE: Only Absolute paths should be given at this stage)
    
    Next: Will implement the create_zip() method

--- zip_creator.py
+++ zip_creator.py
@@ -6,7 +6,24 @@ import logging
 
 class ZipCreator:
     def __init__(self, source, destination):
-       pass
+        """ Initializing the parameters - Source, Destination, Logging"""
+        # Logging parameters:
+        date_suffix = datetime.today().strftime('%d%m%Y')
+        LOG_FILENAME = 'logs\\zip_creator_' + date_suffix + '.log'
+        LOG_FORMAT = "[%(asctime)s] - [ %(levelname)s ]  | %(funcName)s | %(message)s"
+        LOG_LEVEL = logging.DEBUG
+        logging.basicConfig(filename=LOG_FILENAME,
+                            level=LOG_LEVEL,
+                            format=LOG_FORMAT)
+        # Get a Root Logger Object with above config
+        self.logger = logging.getLogger()
+        self.logger.info('--NEW RUN--')
+
+        # Source path format expected: folder\filename.extension
+        self.source_path = source
+        # Destination path format expected: folder\filename.zip
+        self.destination_path = destination
+        self.logger.info('Source/Destination Paths initialized')
 
     def create_zip(self):
       pass
