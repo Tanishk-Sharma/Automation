50750c3113ce05edb2ec81f73ba65a129060d966
Tanishk Sharma <tanishk9597@gmail.com>
2020-08-26 22:58:14 +0530

    Created the ftp_upload.py file
    Added some boilerplate code. Will fill in the method implementations next...

---
+++ ftp_upload.py
@@ -0,0 +1,18 @@
+import ftplib
+import logging
+from datetime import datetime
+
+
+class FTPUpload:
+    def __init__(self, host, user, pwd, zipfile_path):
+        pass
+
+    def connect_to_FTP(self):
+        pass
+
+    def upload_to_FTP(self):
+        pass
+
+    def close_connection_to_FTP(self):
+        pass
+
